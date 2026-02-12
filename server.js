422fd76e436408bbb1e97c2f14d2e54afaf8644c
AzDevcoder <adm.azdevcoder@gmail.com>
2026-02-11 22:04:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || "https://azdevcoder.github.io";
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || "http://psigiovana.azdevcoder.com.br/contrato.html";
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const GITHUB_REPO = process.env.GITHUB_REPO || "sistema_giovana/contratos/contratos-assinados";
+const GITHUB_REPO = process.env.GITHUB_REPO || "/sistema_giovana/contratos/contratos-assinados";
 const GITHUB_BRANCH = process.env.GITHUB_BRANCH || "main";
 
 if (!GITHUB_TOKEN) {
