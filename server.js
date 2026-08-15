cba69553e47fd602c22af092771f53bbe9f6f0f2
AzDevcoder <adm.azdevcoder@gmail.com>
2026-08-15 01:10:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const GITHUB_BRANCH = process.env.GITHUB_BRANCH || "main";
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 async function salvarNoGithub(path, conteudoBase64, mensagem) {
     const url = `https://api.github.com/repos/${GITHUB_REPO}/contents/${path}`;
